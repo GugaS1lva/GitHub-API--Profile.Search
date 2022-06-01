b869316d46cc5a4651910f12ae68d78749b8cfdd
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-06-01 03:08:25 -0300

    Refatoração [002] -
    Criação de Objetos pro Usuário + Divisão de Responsabilidades Aprimorada

---
+++ src/scripts/objects/screen.js
@@ -0,0 +1,29 @@
+const screen = {
+    userProfile: document.querySelector('.profile-data'),
+
+    renderUser(user){
+        this.userProfile.innerHTML = `
+        <div class="info">
+            <img src="${user.avatarUrl}" alt="Foto do Perfil do Usuário">
+            <div class="data">
+                <h1>${user.name ?? 'Não possui nome cadastrado 😭!'}</h1>
+                <p>${user.bio ?? 'Não possui nome cadastrado 😭!'}</p>
+            </div>
+        </div>`
+
+        let repositoriesItems = ''
+        user.repositories.forEach(repo => repositoriesItems += `<li> <a href="${repo.html_url}" target="_blank" >${repo.name}</a> </li>`)
+        console.log(repositoriesItems)
+     
+        if(user.repositories.length > 0){
+            this.userProfile.innerHTML += `
+            <div class="repositories section">
+                <h2>Repositórios</>
+
+                <ul>${repositoriesItems}</>
+            </div>`
+        }
+    }
+}
+
+export {screen}
